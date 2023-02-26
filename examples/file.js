e4e51f944d1bef78223610b11004ece1fd409e19
shiwenzhe <shiwenzhe@lixiang.com>
2023-02-26 09:28:58 +0800

    record bili video fn

--- examples/file.js
+++ examples/file.js
@@ -1,6 +1,9 @@
 const { 
-	launch 
+	launch, 
+	getStream
 } = require('../dist/PuppeteerStream')
+const fs = require('fs')
+const path = require('path')
 
 async function test() {
 	const browser = await launch({
@@ -15,7 +18,33 @@ async function test() {
 			`--remote-debugging-port=8088`
 		]
 	});
-	console.log('running')
+
+	await browser.videoCaptureExtension.exposeFunction("recordBilibili", async (url, fileName, time) => {
+		console.log("begin", url, fileName, time);
+		const page = await browser.newPage();
+		await page.goto(url);
+
+		if (page.locator('.bpx-player-ctrl-muted-icon').isVisible()) {
+			page.getByRole('button', { name: '音量', exact: true }).click()
+		}
+		page.getByRole('button', { name: '全屏', exact: true }).click()
+
+		const recordStream = await getStream(page, {
+			audio: true, 
+			video: true,
+			frameSize: 5 * 1000
+		})
+		const writeStream = fs.createWriteStream(path.resolve(__dirname, fileName))
+		recordStream.pipe(writeStream)
+
+		setTimeout(async () => {
+			await recordStream.destroy();
+			writeStream.close();
+			console.log("finished", url, fileName, time);
+			// await page.close()
+		}, 60 * 1000 * time);
+	});
+
 }
 
 test();
